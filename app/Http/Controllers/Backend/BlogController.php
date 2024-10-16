033a7a073e1139095ce5a72c3b78475afb336f48
dinasija <r.dinasija23@gmail.com>
2024-10-16 11:19:22 +0700

    tugas 7 bagian 2 menjelaskan fungsi

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -28,15 +28,20 @@ class BlogController extends Controller
             'tittle'=>$request->tittle,
             'description'=>$request->description,
             'slug'=>Str::slug($request->tittle),
+            // slug digunakan dalam proses penyimpanan data ke dalam basis data (database) untuk menghasilkan slug dari judul atau teks tertentu.
             'created_by'=>0,
         ];
 
         if ($request->hasFile('file')) {
+            // Memeriksa apakah dalam permintaan HTTP terdapat file dengan nama input
             $file = $request->file('file');
+            // digunakan untuk mengambil file yang diunggah melalui form di Laravel
             $filename = time(). '.' .$file->getClientOriginalExtension();
+            // digunakan untuk membuat nama file unik ketika file diunggah, dengan mempertahankan ekstensi asli file. Metode getClientOriginalExtension() dari objek UploadedFile mengembalikan ekstensi asli file yang diunggah oleh pengguna.
             $file->move(public_path('blogs'), $filename);
-        
+            // digunakan untuk memindahkan file yang diunggah ke direktori yang ditentukan (dalam hal ini, direktori 'blogs' di direktori public). Metode move() dari objek UploadedFile memindahkan file yang diunggah ke lokasi yang ditentukan dan memberikan nama file yang baru.
             $data['file'] = 'blogs/' . $filename;
+            // digunakan untuk menyimpan path file yang baru dihasilkan ke dalam array $data, yang akan digunakan untuk menyimpan informasi file ke dalam basis data.   
         }
         Blogs::insert($data);
         return redirect()->route('backend.blog')->with('success', 'blog berhasil ditambahkan');
